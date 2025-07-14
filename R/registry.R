f48126d7fbfa98c04efc7cd7ecb7a2efa22980b2
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-07-14 17:36:48 +0200

    Register block

--- R/registry.R
+++ R/registry.R
@@ -1,8 +1,8 @@
 register_dataeditr_blocks <- function() {
   blockr.core::register_blocks(
-    c(),
-    name = c(),
-    description = c(),
+    ctor = c("new_data_edit_block"),
+    name = c("Data Edit Block"),
+    description = c("Edit a data frame"),
     package = utils::packageName(),
     overwrite = TRUE
   )
