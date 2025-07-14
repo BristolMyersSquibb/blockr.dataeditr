ed4d9538059e58b3a3c32d288dd920885a6a1cbe
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-07-14 16:49:54 +0200

    Add fun template

---
+++ R/edit.R
@@ -0,0 +1,12 @@
+new_data_edit_block <- function() {
+  ui <- function() {}
+
+  server <- function() {}
+
+  new_dataeditr_block(
+    ui = ui,
+    server = server,
+    class = "data_edit",
+    allow_empty_state = TRUE
+  )
+}
